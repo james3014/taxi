2a40da361557a81d72f1f373e36d50178d4facd4
james3014 <grantj3014@gmail.com>
2020-05-04 12:18:03 +0100

    Initial Commit.

---
+++ src/main/java/ac/uk/strathclyde/App.java
@@ -0,0 +1,8 @@
+package ac.uk.strathclyde;
+
+public class App {
+
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/test/java/ac/uk/strathclyde/AppTest.java
@@ -0,0 +1,13 @@
+package ac.uk.strathclyde;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
