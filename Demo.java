eaffe0ee0f3d4f165653fe78921270ebde92d71d
YuliaCS112 <64264699+YuliaCS112@users.noreply.github.com>
2020-05-13 16:05:57 +0100

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,67 @@
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Provide a simple demonstration of running a stage-one
+ * scenario. A single passenger is created, and a pickup
+ * requested. As the simulation is run, the passenger
+ * should be picked up and then taken to their destination.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class Demo
+{
+    private List<Actor> actors;
+    private int step;
+
+    /**
+     * Constructor for objects of class Demo
+     */
+    public Demo()
+    {
+        actors = new LinkedList<>();
+        step = 0;
+        reset();
+    }
+    
+    /**
+     * Run the demo for a fixed number of steps.
+     */
+    public void run()
+    {
+        System.out.println("Welcome to our application!");
+        for(int i = 0; i < 500; i++){
+            step++;
+            step();
+        }
+        System.out.println("All journeys finished.");
+    }
+
+    /**
+     * Run the demo for one step by requesting
+     * all actors to act.
+     */
+    public void step()
+    {
+        for(Actor actor : actors) {
+            actor.act();
+        }
+    }
+    
+    /**
+     * Reset the demo to a starting point.
+     * A single taxi is created, and a pickup is
+     * requested for a single passenger.
+     * @throws IllegalStateException If a pickup cannot be found
+     */
+    public void reset()
+    {
+        actors.clear();
+        City city = new City();
+        TaxiCompany company = new TaxiCompany(city);
+        PassengerSource source = new PassengerSource(city, company);
+        actors.addAll(company.getVehicles());
+        actors.add(source);
+    }
+}
---
+++ PassengerSource.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+/**
+ * Periodically generate passengers.
+ * Keep track of the number of passengers for whom
+ * a vehicle cannot be found.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class PassengerSource implements Actor
+{
+    private City city;
+    private TaxiCompany company;
+    private Random rand;
+    private static final double CREATION_PROBABILITY = 0.06;
+    private int missedPickups;
+
+    /**
+     * Constructor for objects of class PassengerSource.
+     * @param city The city. Must not be null.
+     * @param company The company to be used. Must not be null.
+     * @throws NullPointerException if city or company is null.
+     */
+    public PassengerSource(City city, TaxiCompany company)
+    {
+        if(city == null) {
+            throw new NullPointerException("city");
+        }
+        if(company == null) {
+            throw new NullPointerException("company");
+        }
+        this.city = city;
+        this.company = company;
+        // Use a fixed random seed for repeatable effects.
+        // Change this to produce more random effects.
+        rand = new Random(12345);
+        missedPickups = 0;
+    }
+
+    /**
+     * Randomly generate a new passenger.
+     * Keep a count of missed pickups.
+     */
+    public void act()
+    {
+        if(rand.nextDouble() <= CREATION_PROBABILITY) {
+            List<Passenger> passengers = new ArrayList<>();
+            passengers.add(createPassenger());
+            passengers.add(createPassenger());
+            if(company.requestPickup(passengers)) {
+               for (Passenger p : passengers)
+               {
+                   city.addItem(p);
+               }
+            }
+            else {
+                missedPickups++;
+            }
+        }
+    }
+
+    /**
+     * @return The number of passengers for whom a pickup
+     *         could not be found.
+     */
+    public int getMissedPickups()
+    {
+        return missedPickups;
+    }
+
+    /**
+     * Create a new passenger with distinct pickup and
+     * destination locations.
+     * @return The created passenger.
+     */
+    private Passenger createPassenger()
+    {
+        int cityWidth = city.getWidth();
+        int cityHeight = city.getHeight();
+
+        Location pickupLocation =
+                    new Location(rand.nextInt(cityWidth),
+                                 rand.nextInt(cityHeight));
+        Location destination;
+        do{
+            destination =
+                    new Location(rand.nextInt(cityWidth),
+                                 rand.nextInt(cityHeight));
+        } while(pickupLocation.equals(destination));
+        return new Passenger(pickupLocation, destination, generatePassengerName());
+    }
+
+    /**
+     * Pick a random name from an array of names
+     * @return passenger name
+     */
+    private String generatePassengerName() {
+        String[] names = {"Truffle", "Wilfred", "passenger 3", "passenger 4", "passenger 5"};
+        Random rn = new Random();
+        int i = rn.nextInt(5);
+        return names[i];
+    }
+}
