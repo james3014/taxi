072bc666e75fe2c683ccd5a33c3ff5e4c2d49501
CharlieMuir19 <charliemuir2141@gmail.com>
2020-05-14 18:21:46 +0100

    Add new City class in strath package

---
+++ src/main/java/ac/uk/strathclyde/City.java
@@ -0,0 +1,16 @@
+package ac.uk.strathclyde;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A collection of items in the city.
+ *
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class City
+{
+
+}
